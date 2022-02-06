147428324367567e8e9782bbc8c926c743475e7e
LJ <ljlodl5@att.net>
2022-02-06 15:14:08 -0600

    Updated goldcopy, new python and sqlite files

---
+++ AdditionalDatabaseSets/model.py
@@ -0,0 +1,20 @@
+from sqlite_guide import engine
+engine.execute(
+    '''CREATE TABLE IF NOT EXISTS 
+        user_input_java_winner (
+        id INTEGER NOT NULL,
+        username VARCHAR, 
+        a_points INTEGER NOT NULL,
+        b_points INTEGER NOT NULL,
+        a_offense_rush_yards INTEGER NOT NULL,
+        b_offense_rush_yards INTEGER NOT NULL,
+        a_incomplete_pass INTEGER NOT NULL,
+        b_incomplete_pass INTEGER NOT NULL,
+        a_penalty INTEGER NOT NULL,
+        b_penalty INTEGER NOT NULL,
+        a_penalty_yards INTEGER NOT NULL,
+        b_penalty_yards INTEGER NOT NULL,
+        surface INTEGER NOT NULL,
+        b_home INTEGER NOT NULL,
+        result INTEGER NOT NULL,
+        PRIMARY KEY (id))''' )
